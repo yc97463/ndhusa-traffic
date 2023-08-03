0aeb8024fff7869f0e1fbe54ba93629a4bcee32d
yc97463 <hi@imych.one>
2023-08-03 18:05:28 +0800

    add map page

---
+++ pages/map.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "@/components/map";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="h-full">
+      <Map />
+    </div>
+  );
+};
+
+export default HomePage;
