2376f016a30c336d3d73ee3283574f8077617142
yc97463 <hi@imych.one>
2023-08-03 13:45:43 +0800

    use 非二元性別

--- pages/index.tsx
+++ pages/index.tsx
@@ -325,7 +325,9 @@ export default function Survey() {
                   className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
                 >
                   <div className="block">
-                    <div className="w-full text-lg font-semibold">其他</div>
+                    <div className="w-full text-lg font-semibold">
+                      非二元性別
+                    </div>
                     <div className="w-full">Other</div>
                   </div>
                   <svg
