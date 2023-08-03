7aada6ed3a7877cb60d5b4f716f4d5f5b35bc08a
yc97463 <hi@imych.one>
2023-08-03 15:43:56 +0800

    init login page

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <>
+      <div className="container max-w-sm mx-auto">
+        <h1 className="text-center text-lg font-medium my-4">請登入</h1>
+        <div className="flex justify-center my-6">
+          <div className="m-auto max-w-md">Sign in with Google</div>
+        </div>
+      </div>
+    </>
+  );
+}
