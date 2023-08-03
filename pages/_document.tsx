076067c7b611febe5695a01747fd689ce731033e
yc97463 <hi@imych.one>
2023-08-03 18:05:15 +0800

    add Google Fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html lang="zh-TW">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
