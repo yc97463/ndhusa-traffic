c9dca9fe4dcbf2a4e3c361ebb0949f80e25c0141
yc97463 <hi@imych.one>
2023-12-28 01:54:53 +0800

    feat: add redirect from `/` to `/landing`

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export',}
+const nextConfig = {
+    // output: 'export', 
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/landing',
+                permanent: true,
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
