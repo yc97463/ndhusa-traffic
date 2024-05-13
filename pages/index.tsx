95f064396eb268b775bd3c111e9da4fc345806e6
yc97463 <hi@imych.one>
2024-05-14 03:34:21 +0800

    feat index: use Google Fonts meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,8 @@ export default function Home() {
         <link rel="icon" href="/img/ndhusa-logo-favicon.png" type="image/x-icon" />
         <link rel="bookmark" href="/img/ndhusa-logo-favicon.png" type="image/x-icon" />
         <link rel="apple-touch-icon-precomposed" href="/img/ndhusa-logo-favicon.png"></link>
-        {/* ogimage */}
+
+        {/* meta */}
         <meta property="og:title" content="第 13 屆學權議題儀表板 - 東華學生會議題百寶箱" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://dashboard.dhsa.ndhu.me" />
@@ -23,6 +24,11 @@ export default function Home() {
         <meta property="og:description" content="東華學生會第 13 屆學權議題儀表板" />
         <meta property="og:site_name" content="東華學生會議題百寶箱" />
         <meta property="og:locale" content="zh_TW" />
+
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans+TC:wght@100..900&family=Noto+Serif+TC&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </Head>
       <Navbar />
       <div className="container mx-auto intro">
