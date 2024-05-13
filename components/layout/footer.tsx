65a9c9117aa78a9076e829581e8eea8598b893d1
yc97463 <hi@imych.one>
2024-05-14 04:10:28 +0800

    feat component/footer: adjest footer links

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -8,12 +8,11 @@ function Footer() {
             <div className="flex justify-between items-center h-24 w-[100vw-24px] mx-auto lg:max-w-[1440px] ">
                 <div className="title">
                     <Link href="/">
-                        <Image src="/img/logo-and-title.svg" alt="logo" width={289} height={31} />
+                        首頁
                     </Link>
                 </div>
                 <div>
                     <Link href="https://imych.one">❤️ by @yc97463</Link>
-                    <Link href="https://github.com/yc97463/ndhusa13th-dashboard">Github</Link>
                 </div>
             </div>
         </nav>
