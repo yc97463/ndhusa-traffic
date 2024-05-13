c36c5f9dda4bce7cd17c5eb61fc861f87aa777d8
yc97463 <hi@imych.one>
2024-05-13 23:43:52 +0800

    feat index: redirect path to `/landing`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
-import { redirect } from 'next/navigation';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default async function GoLanding() {
-    redirect('/landing');
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to "/landing" when the component mounts
+    router.push("/landing");
+  }, []); // Empty dependency array means this effect runs only once after the component mounts
+
+  return <></>; // You can return any JSX here if needed
 }
