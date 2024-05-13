91825ac1c89654c9fc3cfdcf2a1b11baf46f944d
yc97463 <hi@imych.one>
2024-05-14 03:05:11 +0800

    feat index: init dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,91 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Footer from "@/components/layout/footer";
+import Navbar from "@/components/layout/navbar";
 
 export default function Home() {
-  const router = useRouter();
 
-  useEffect(() => {
-    // Redirect to "/landing" when the component mounts
-    router.push("/landing");
-  }, []); // Empty dependency array means this effect runs only once after the component mounts
-
-  return <></>; // You can return any JSX here if needed
+  return (
+    <div className="">
+      <Navbar />
+      <div className="container mx-auto intro">
+        <p>東華學生會</p>
+        <p>第 13 屆學權議題儀表板</p>
+      </div>
+      <div className="lg:container bg-white mx-4 my-4 py-4 min-h-fit lg:w-full lg:mx-auto lg:h-64">
+        <div className="highlight grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4">
+          <div className="card">
+            <h2>正在進行</h2>
+            <div className="grid grid-cols-1">
+              <p className="big-number">5</p>
+              <p className="text-center font-bold">個議題</p>
+            </div>
+          </div>
+          <div className="card">
+            <h2>熱度標籤</h2>
+            <p className="hashtag">#宿舍 #選舉 #遠距教學 #地震</p>
+          </div>
+          <div className="card">
+            <h2>已完成</h2>
+            <div className="grid grid-cols-1">
+              <p className="big-number">10</p>
+              <p className="text-center font-bold">項領域</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto my-8">
+        <h1 className="text-3xl text-center my-4">我們的議題</h1>
+        <div className="issue grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="card">
+            <h2>宿舍登記表單</h2>
+            <p className="hashtag">#宿舍 #調查</p>
+            <div className="grid grid-cols-1">
+              <p className="progress">57% 完成</p>
+              {/* progress bar */}
+            </div>
+          </div>
+          <div className="card">
+            <h2>宿舍登記表單</h2>
+            <p className="hashtag">#宿舍 #調查</p>
+            <div className="grid grid-cols-1">
+              <p className="progress">57% 完成</p>
+              {/* progress bar */}
+            </div>
+          </div>
+          <div className="card">
+            <h2>宿舍登記表單</h2>
+            <p className="hashtag">#宿舍 #調查</p>
+            <div className="grid grid-cols-1">
+              <p className="progress">57% 完成</p>
+              {/* progress bar */}
+            </div>
+          </div>
+          <div className="card">
+            <h2>宿舍登記表單</h2>
+            <p className="hashtag">#宿舍 #調查</p>
+            <div className="grid grid-cols-1">
+              <p className="progress">57% 完成</p>
+              {/* progress bar */}
+            </div>
+          </div>
+          <div className="card">
+            <h2>宿舍登記表單</h2>
+            <p className="hashtag">#宿舍 #調查</p>
+            <div className="grid grid-cols-1">
+              <p className="progress">57% 完成</p>
+              {/* progress bar */}
+            </div>
+          </div>
+          <div className="card">
+            <h2>宿舍登記表單</h2>
+            <p className="hashtag">#宿舍 #調查</p>
+            <div className="grid grid-cols-1">
+              <p className="progress">57% 完成</p>
+              {/* progress bar */}
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
