ec31e6da65701a58dcd771f002f7e07ae313840a
yc97463 <hi@imych.one>
2024-07-17 21:38:33 +0800

    feat component/proj/card: add field & refactor code
    - props
    - topic
    - describe
    - AsyncImage component

--- components/project/card.tsx
+++ components/project/card.tsx
@@ -1,16 +1,27 @@
 import Link from "next/link";
 import React from "react";
+import AsyncImage from "@/components/AsyncImage";
 
+type ProjectCardProps = {
+    title: string;
+    topic?: string;
+    hashtag: string;
+    describe?: string;
+    progress: number;
+    imgSrc: string;
+    href: string;
+  };
+  
 
-
-function ProjectCard({ title, hashtag, progress, imgSrc, href }: { title: string, hashtag: string, progress: number, imgSrc: string, href: string }) {
+function ProjectCard({ title, topic, hashtag, describe, progress, imgSrc, href }: ProjectCardProps) {
     return (
         <Link href={`/project/` + href}>
             <div className="card">
                 <h2>{title}</h2>
                 <p className="hashtag">{hashtag}</p>
-                {imgSrc && <img src={imgSrc} className="w-full" />}
+                {imgSrc && <AsyncImage src={imgSrc} alt={title} placeholderSrc="/img/og.jpg" className="w-full" />}
                 <div className="grid grid-cols-1">
+                    <p>{describe}</p>
                     <p className="progress">{progress}% 完成</p>
                     {/* progress bar */}
                 </div>
